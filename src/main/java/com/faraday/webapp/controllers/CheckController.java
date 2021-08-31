517ea2b44cc72cad65bbe420297baf9d39598683
paolopac <paoloacqua@hotmail.it>
2021-08-31 17:25:11 +0200

    NEW END POINT /check/health READY.
    TEST DOCKER-COMPOSE: OK

--- src/main/java/com/faraday/webapp/controllers/CheckController.java
+++ src/main/java/com/faraday/webapp/controllers/CheckController.java
@@ -1,12 +1,9 @@
 package com.faraday.webapp.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.faraday.webapp.models.InfoWS;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,13 +20,17 @@ import lombok.extern.java.Log;
 public class CheckController {
 
   @Autowired
-  InfoWS infoWS;
+	private ResourceBundleMessageSource messager; 
 
-	@RequestMapping(value="/info", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<InfoWS> getInfo() {
+  @Value("${spring.application.name}")
+  private String appName;
 
-    log.info("********** Get webservice info ");
-    return new ResponseEntity<InfoWS>(infoWS, HttpStatus.OK);
+	@RequestMapping(value="/health", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Void> getHealth() {
+
+    log.info(String.format(messager.getMessage("Ok.Check.health.ping", null, LocaleContextHolder.getLocale()), appName));
+
+    return new ResponseEntity<>(HttpStatus.OK);
 
 	}
 
---
+++ src/test/java/com/faraday/webapp/ControllerTest/CRUDTests/CheckControllerTests.java
@@ -0,0 +1,60 @@
+package com.faraday.webapp.ControllerTest.CRUDTests;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.faraday.webapp.Application;
+import com.faraday.webapp.entities.Associations;
+import com.faraday.webapp.repository.AssociationRepository;
+
+@SpringBootTest
+@ActiveProfiles({"test"})
+@ContextConfiguration(classes = Application.class)
+public class CheckControllerTests {
+  
+  private MockMvc mockMvc;
+
+  @Autowired
+	private WebApplicationContext wac;
+
+  @BeforeEach
+	public void setup() throws JSONException, IOException
+	{
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+
+  @Test
+  @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+  public void testCheckHealth() throws Exception {
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/check/health")
+    .accept(MediaType.APPLICATION_JSON))
+    .andExpect(status().isOk())
+    .andReturn();
+
+  }
+
+}
