669e97543172e6c89db0a2c6a2bba31bd981cf2e
paolopac <paoloacqua@hotmail.it>
2021-09-04 11:53:26 +0200

    UPGRADE VERSIONE: 0.3.0-SNAPSHOT
    MAIN:
    rimosso eureka client
    
    DEBUG MODE:
    configurato affinché, all’avvio in modalità debug riceva in input le variabili ad esso necesaarie.
    
    PROPERTIES:
    zone1:
    rimosso eureka;
    configurato affinché le chiamate ribbon contattino ZUUL-API-GATWAY
    test:
    rimosso eureka;
    dev:
    rimosso.
    
    POM:
    rimosso eureka

--- src/main/java/com/faraday/webapp/Application.java
+++ src/main/java/com/faraday/webapp/Application.java
@@ -3,12 +3,12 @@ package com.faraday.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 
 @EnableCaching
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
